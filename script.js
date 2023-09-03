98a9a646fafddb5851152f1f4b5026806c133206
Gustavo Carvalho <41920575+GustavoCarvalho1592@users.noreply.github.com>
2023-09-03 20:24:53 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+//function carregar() {
+   ///const meuBotao = document.getElementById('meuBotao') 
+  // Array de nomes de imagens aleatórias
+  //const imagensAleatorias = ["/EX01/img/tristeza2.jpg", "/EX01/img/tristeza3.jpg", "/EX01/img/tristeza4.jpg", "/EX01/img/tristeza5.jpg", "/EX01/img/tristeza6.jpg"];
+
+  // Gerar um número aleatório de 0 a 4
+  //const numeroAleatorio = Math.floor(Math.random() * imagensAleatorias.length);
+    
+  // Selecionar aleatoriamente uma imagem da lista de imagens aleatórias
+  //const imagemAleatoria = imagensAleatorias[numeroAleatorio];
+
+  // Exibir a imagem aleatória no elemento com ID "imagem"
+  //document.getElementById("imagem").src = imagemAleatoria;
+
+
+
+}
