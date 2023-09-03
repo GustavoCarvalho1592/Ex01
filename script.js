3e5558a9b25d8fd242c806669c764b3a66a82eba
Wallysson Vieira <127979077+mvwall@users.noreply.github.com>
2023-09-03 20:43:49 -0300

    comit - imagens

--- script.js
+++ script.js
@@ -1,18 +1,121 @@
 
-//function carregar() {
-   ///const meuBotao = document.getElementById('meuBotao') 
-  // Array de nomes de imagens aleatórias
-  //const imagensAleatorias = ["/EX01/img/tristeza2.jpg", "/EX01/img/tristeza3.jpg", "/EX01/img/tristeza4.jpg", "/EX01/img/tristeza5.jpg", "/EX01/img/tristeza6.jpg"];
+function trizteza() {
+  const meuBotao = document.getElementById('meuBotao') 
+  //Array de nomes de imagens aleatórias
 
-  // Gerar um número aleatório de 0 a 4
-  //const numeroAleatorio = Math.floor(Math.random() * imagensAleatorias.length);
+  const imagensAleatorias = ["/EX01/img/tristeza2.jpg", "/EX01/img/tristeza3.jpg", 
+  "/EX01/img/tristeza4.jpg", "/EX01/img/tristeza5.jpg", "/EX01/img/tristeza6.jpg"];
+
+  //Gerar um número aleatório de 0 a 4
+  const numeroAleatorio = Math.floor(Math.random() * imagensAleatorias.length);
     
-  // Selecionar aleatoriamente uma imagem da lista de imagens aleatórias
-  //const imagemAleatoria = imagensAleatorias[numeroAleatorio];
+  //Selecionar aleatoriamente uma imagem da lista de imagens aleatórias
+  const imagemAleatoria = imagensAleatorias[numeroAleatorio];
 
-  // Exibir a imagem aleatória no elemento com ID "imagem"
-  //document.getElementById("imagem").src = imagemAleatoria;
-
+    //Exibir a imagem aleatória no elemento com ID "imagem"
+    document.getElementById("imagem").src = imagemAleatoria;
 
 
 }
+
+function ansiedade() {
+  const meuBotao = document.getElementById('meuBotao') 
+  //Array de nomes de imagens aleatórias
+
+  const imagensAleatorias = ["/EX01/img/ansiedade-1.jpg", "/EX01/img/ansiedade-2.jpg", 
+  "/EX01/img/ansiedade-3.jpg", "/EX01/img/ansiedade-4.jpg", "/EX01/img/ansiedade-5.jpg"];
+
+  //Gerar um número aleatório de 0 a 4
+  const numeroAleatorio = Math.floor(Math.random() * imagensAleatorias.length);
+    
+  //Selecionar aleatoriamente uma imagem da lista de imagens aleatórias
+  const imagemAleatoria = imagensAleatorias[numeroAleatorio];
+
+    //Exibir a imagem aleatória no elemento com ID "imagem"
+    document.getElementById("imagem").src = imagemAleatoria;
+
+
+}
+
+
+function alegria() {
+  const meuBotao = document.getElementById('meuBotao') 
+  //Array de nomes de imagens aleatórias
+
+  const imagensAleatorias = ["/EX01/img/alegria-1.jpg", "/EX01/img/alegria-2.jpg", 
+  "/EX01/img/alegria-3.jpg", "/EX01/img/alegria-4.jpg", "/EX01/img/alegria-5.jpg"];
+
+  //Gerar um número aleatório de 0 a 4
+  const numeroAleatorio = Math.floor(Math.random() * imagensAleatorias.length);
+    
+  //Selecionar aleatoriamente uma imagem da lista de imagens aleatórias
+  const imagemAleatoria = imagensAleatorias[numeroAleatorio];
+
+    //Exibir a imagem aleatória no elemento com ID "imagem"
+    document.getElementById("imagem").src = imagemAleatoria;
+
+
+}
+
+
+function medo() {
+  const meuBotao = document.getElementById('meuBotao') 
+  //Array de nomes de imagens aleatórias
+
+  const imagensAleatorias = ["/EX01/img/medo-1.jpg", "/EX01/img/medo-2.jpg", 
+  "/EX01/img/medo-3.jpg", "/EX01/img/medo-4.jpg", "/EX01/img/medo-5.jpg"];
+
+  //Gerar um número aleatório de 0 a 4
+  const numeroAleatorio = Math.floor(Math.random() * imagensAleatorias.length);
+    
+  //Selecionar aleatoriamente uma imagem da lista de imagens aleatórias
+  const imagemAleatoria = imagensAleatorias[numeroAleatorio];
+
+    //Exibir a imagem aleatória no elemento com ID "imagem"
+    document.getElementById("imagem").src = imagemAleatoria;
+
+
+}
+
+
+function naosei() {
+  const meuBotao = document.getElementById('meuBotao') 
+  //Array de nomes de imagens aleatórias
+
+  const imagensAleatorias = ["/EX01/img/Nao-Sei-1.jpg", "/EX01/img/Nao-Sei-2.jpg", 
+  "/EX01/img/Nao-Sei-3.jpg", "/EX01/img/Nao-Sei-4.jpg", "/EX01/img/Nao-Sei-5.jpg"];
+
+  //Gerar um número aleatório de 0 a 4
+  const numeroAleatorio = Math.floor(Math.random() * imagensAleatorias.length);
+    
+  //Selecionar aleatoriamente uma imagem da lista de imagens aleatórias
+  const imagemAleatoria = imagensAleatorias[numeroAleatorio];
+
+    //Exibir a imagem aleatória no elemento com ID "imagem"
+    document.getElementById("imagem").src = imagemAleatoria;
+
+
+}
+
+function raiva() {
+  const meuBotao = document.getElementById('meuBotao') 
+  //Array de nomes de imagens aleatórias
+
+  const imagensAleatorias = ["/EX01/img/Raiva-1.jpg", "/EX01/img/Raiva-2.jpg", 
+  "/EX01/img/Raiva-3.jpg", "/EX01/img/Raiva-4.jpg", "/EX01/img/Raiva-5.jpg"];
+
+  //Gerar um número aleatório de 0 a 4
+  const numeroAleatorio = Math.floor(Math.random() * imagensAleatorias.length);
+    
+  //Selecionar aleatoriamente uma imagem da lista de imagens aleatórias
+  const imagemAleatoria = imagensAleatorias[numeroAleatorio];
+
+    //Exibir a imagem aleatória no elemento com ID "imagem"
+    document.getElementById("imagem").src = imagemAleatoria;
+
+
+}
+
+
+
+
